745ce4dffe8da46d7c5323ec160915a0d2726eb1
AlecHero <alexander.wittrup@gmail.com>
2025-04-10 19:39:06 +0200

    add time registration skeleton created

---
+++ src/test/java/acceptance_tests/AddTimeRegistrationSteps.java
@@ -0,0 +1,44 @@
+package acceptance_tests;
+
+import dtu.time_manager.app.Activity;
+import dtu.time_manager.app.Project;
+import dtu.time_manager.app.TimeManager;
+import dtu.time_manager.app.User;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AddTimeRegistrationSteps {
+    @Given("an activity exists")
+    public void anActivityExists() {
+
+    }
+    @When("the user selects an activity")
+    public void theUserSelectsAnActivity() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("enters a valid number of hours")
+    public void entersAValidNumberOfHours() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("enters a valid date")
+    public void entersAValidDate() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the Time Registration is added")
+    public void theTimeRegistrationIsAdded() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
