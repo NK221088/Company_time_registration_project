a24e144c4c8e99fc08ca57ca0280458335541d8a
Isak <s224176@dtu.dk>
2025-04-22 15:37:46 +0200

    viewActivity created

--- src/main/java/dtu/time_manager/app/Activity.java
+++ src/main/java/dtu/time_manager/app/Activity.java
@@ -1,8 +1,14 @@
 package dtu.time_manager.app;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.*;
+
 public class Activity {
 
     private String ActivityName;
+    private ArrayList<User> users = new ArrayList<>();
+    private Long expectedWorkHours;
 
     // Constructor with project name
     public Activity(String ActivityName) {
@@ -10,6 +16,19 @@ public class Activity {
     }
 
     public String getActivityName() {
-        return ActivityName;
+        return this.ActivityName;
     }
+    public ArrayList<User> getUsers() { return this.users;}
+    public Long getExpectedWorkHours() { return this.expectedWorkHours;}
+
+    public Map<String, Object> viewActivity() {
+        Map<String,Object> info = new HashMap();
+        info.put("Name", getActivityName());
+        info.put("Users", getUsers());
+        info.put("ExpectedWorkHours", getExpectedWorkHours());
+
+        return info;
+    }
+
 }
+
