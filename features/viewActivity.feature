e30889993a6a578ff93eb293a4ac4290518fe9ce
Isak <s224176@dtu.dk>
2025-05-01 14:55:47 +0200

    viewActivitySteps completed

--- features/viewActivity.feature
+++ features/viewActivity.feature
@@ -6,7 +6,7 @@ Feature: View Activity
     Given a user is logged in
     And a project with project ID "25001" and project name "Project 1" and time interval "2025-01-01 - 2025-01-08" exists in the system
     And that the project with project ID "25001" have a registered activity with name "Activity 1"
-    And the activity has the start date "11-2025" and end date "25-2025"
+    And the activity has the start date "2025-01-11" and end date "2025-01-25"
     When the user views the project with project ID "25001"
     And select an activity with name "Activity 1" from project ID "25001"
     Then the activity name of "Activity 1" is shown
@@ -14,7 +14,8 @@ Feature: View Activity
     And the number of work hours of "3" hours is spent on "Activity 1" is shown
     And the start date is shown
     And the end date is shown
+    And the initials of the developer or developers "huba" working on the "Activity 1" is shown
     And the assigned users are shown
     And the users who have worked on the project are shown
-    And the initials of the developer or developers "huba" working on the "Activity 1" is shown
+
 
--- src/test/java/acceptance_tests/viewActivitySteps.java
+++ src/test/java/acceptance_tests/viewActivitySteps.java
@@ -1,6 +1,7 @@
 package acceptance_tests;
 
 import dtu.time_manager.app.*;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -10,7 +11,6 @@ import java.time.LocalDate;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class viewActivitySteps {
 
@@ -20,14 +20,19 @@ public class viewActivitySteps {
     private User assignedUser;
     private TimeRegistration test_time_registration;
     private double workedHours;
+    private LocalDate startDate;
+    private LocalDate endDate;
 
+    @Given("the activity has the start date {string} and end date {string}")
+    public void theActivityHasTheStartDateAndEndDate(String startDate, String endDate) {
+        this.startDate = LocalDate.parse(startDate);
+        this.endDate = LocalDate.parse(endDate);
+    }
 
     @When("select an activity with name {string} from project ID {string}")
     public void selectAnActivityWithNameFromProjectID(String activityName, String projectID) {
         this.test_project = TimeManager.getProjectFromID(projectID);
         this.activity = new Activity(activityName);
-        this.activity.addWorkingUser(this.workingUser);
-
 
 
     }
@@ -49,35 +54,47 @@ public class viewActivitySteps {
 
     @Then("the number of work hours of {string} hours is spent on {string} is shown")
     public void theNumberOfWorkHoursOfHoursIsSpentOnIsShown(String assignedWorkHours, String activityName) {
+        this.workingUser = new User("huba");
         this.test_time_registration = new TimeRegistration(this.workingUser, this.activity, Integer.parseInt(assignedWorkHours), LocalDate.now());
         Map <String, Object> info = this.activity.viewActivity();
         assertEquals(Double.parseDouble(assignedWorkHours), info.get("WorkedHours"));
     }
-//    @Then("the start date is shown")
-//    public void theStartDateIsShown() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    }
-//    @Then("the end date is shown")
-//    public void theEndDateIsShown() {
-//        // Write code here that turns the phrase above into concrete actions
-//        throw new io.cucumber.java.PendingException();
-//    @Then("the assigned users are shown")
-//    public void theAssignedUsersAreShown() {
-//        assertNotNull(this.projectVariables.get("Assigned employees"));
-//    }
-//
-//    @Then("the users who have worked on the project are shown")
-//    public void theUsersWhoHaveWorkedOnTheProjectAreShown() {
-//        assertNotNull(this.projectVariables.get("Contributing users"));
-//    }
+   @Then("the start date is shown")
+    public void theStartDateIsShown() {
+        this.activity.setActivityStartTime(this.startDate);
+        Map<String, Object> info = this.activity.viewActivity();
+        assertEquals(this.activity.getActivityStartTime(), info.get("StartTime"));
+
+
+    }
+    @Then("the end date is shown")
+    public void theEndDateIsShown() {
+        this.activity.setActivityEndTime(this.endDate);
+        Map<String, Object> info = this.activity.viewActivity();
+        assertEquals(this.activity.getActivityEndTime(), info.get("EndTime"));
+    }
 
     @Then("the initials of the developer or developers {string} working on the {string} is shown")
     public void theInitialsOfTheDeveloperOrDevelopersWorkingOnTheIsShown(String user, String activityName) {
         this.activity.assignUser(user);
+        this.activity.addWorkingUser(TimeManager.getUser(user));
         Map <String, Object> info = this.activity.viewActivity();
         ArrayList<User> userList = (ArrayList<User>) info.get("Assigned employees");
         assertTrue(userList.contains(TimeManager.getUser(user)));
     }
+
+    @Then("the assigned users are shown")
+    public void theAssignedUsersAreShown() {
+        Map<String, Object> info = this.activity.viewActivity();
+        assertNotNull(info.get("Assigned employees"));
+    }
+
+    @Then("the users who have worked on the project are shown")
+    public void theUsersWhoHaveWorkedOnTheProjectAreShown() {
+        Map<String, Object> info = this.activity.viewActivity();
+        assertNotNull(info.get("Contributing employees"));
+    }
+
+
 
 }
