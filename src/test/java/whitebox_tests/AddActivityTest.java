219baaab111fb5ccd51a8fdd3cd13a238a5e006e
Isak <s224176@dtu.dk>
2025-05-10 23:24:45 +0200

    AddActivityTest.java implemented

---
+++ src/test/java/whitebox_tests/AddActivityTest.java
@@ -0,0 +1,44 @@
+package whitebox_tests;
+
+import dtu.timemanager.domain.*;
+import java.util.List;
+
+import io.cucumber.java.en_old.Ac;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class AddActivityTest {
+    private TimeManager timeManager;
+
+    @BeforeEach
+    void setUp() {
+        this.timeManager = new TimeManager();
+    }
+
+    @Test
+    void testAddActivitySuccess() throws Exception {
+        Project project = timeManager.addProject("Project A");
+        Activity activity = new Activity("Design");
+        project.addActivity(activity);
+
+        assertNotNull(project);
+        assertNotNull(activity);
+        assertEquals(1, project.getActivities().size());
+        assertSame(activity, project.getActivities().getFirst());
+    }
+
+    @Test
+    void testAddProjectException() throws Exception {
+        Project project = timeManager.addProject("Project A");
+        Activity activity = new Activity("Design");
+        project.addActivity(activity);
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> project.addActivity(activity));
+        String expectedMsg = "An activity with name 'Design' already exists within 'Project A' two activities cannot exist with the same name within the same project.";
+
+        assertEquals(1, project.getActivities().size());
+        assertEquals(expectedMsg, e.getMessage());
+    }
+}
